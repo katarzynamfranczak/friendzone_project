077f4cd1672145444734f2fc9fc92ef84d29f285
Mahum <154818596+mkh-24@users.noreply.github.com>
2024-11-16 19:33:55 +0000

    Update FriendZone.sql
    Updated FriendZone DB, DP & MK

--- FriendZone.sql
+++ FriendZone.sql
@@ -1,56 +1,56 @@
+DROP DATABASE FriendZone;
 CREATE DATABASE FriendZone;
 USE FriendZone;
 -- A table consisting of all users with login credentials for FriendZone:
 CREATE TABLE Users (
-  UserId VARCHAR(5) PRIMARY KEY NOT NULL,
-  UserName VARCHAR(20) NOT NULL,
-  Email VARCHAR(20) NOT NULL,
-  Password VARCHAR(8) NOT NULL),
-  UserType VARCHAR(5) NOT NULL), 
-  CHECK (UserAge>=18) -- A constraint to ensure users are at least 18 years of age to use the app. 
+  user_id VARCHAR(5) PRIMARY KEY NOT NULL, -- known to app admin
+  user_name VARCHAR(20) UNIQUE NOT NULL,
+  email VARCHAR(20) UNIQUE NOT NULL,
+  user_password VARCHAR(10) NOT NULL, -- only accepts 10, not 9? *add in Python code: to be 9 characters long, upper, lower case, number, special character*
+  user_type ENUM("admin", "user") NOT NULL, 
+  user_dob DATE NOT NULL, -- must be in the format: YYYY-MM-DD *instruction to be made in Python code*
+  CONSTRAINT age_restriction CHECK (TIMESTAMPDIFF(YEAR, user_dob, SYSDATE()) >= 18)
 );
 
--- A table consisting of users' searches on FriendZone:
---  (NB Instead of FAVES, Search_Results)
-CREATE TABLE Search_Results (
-  UserId VARCHAR(5) FOREIGN KEY, 
-  LatLon INT NOT NULL, -- needs to be from API
-  Times NOT NULL, -- check Time format/if as timeblocks? 
-  Activity VARCHAR NOT NULL), -- needs to be in line with API
-  VenueId INT, -- unique, useful for later if adding data for REVIEWS
-  Vegan BOOL,
-  AlcoholFree BOOL, 
-  DogFriendly BOOL
-  );
-  
-  CREATE TABLE Messages (
-  UserId VARCHAR(5) FOREIGN KEY NOT NULL, -- maybe mixing up my languages, but maybe have UserId as From_User?
-  ToUser VARCHAR(20) NOT NULL, -- decide if this is UserID or as text? (text is more intuitive, UserId is cleaner?) ...alt: can use join to have both
-  FromUser VARCHAR(20) NOT NULL,
-  DateTime  -- check format type NOT NULL,
-  Message VARCHAR(240) NOT NULL, 
-  Read BOOL NOT NULL); --possibly, this should be separate and instead have a sent? (/additionally)
-
 INSERT INTO Users
-VALUES
-(ID001, "DesiP", "dp@mail.com", "password1", "admin"),
-(ID002, "HannahC", "hc@mail.com", "password2", "admin"),
-(ID003, "KatF", "kf@mail.com", "password3", "user"),
-(ID004, "KoniB", "kb@mail.com", "password4)", "user",
-(ID005, "MahumK", "mk@mail.com", "password5", "user"),
-(ID006, "MariaP", "mp@mail.com", "password6", "user"),
-(ID007, "RahaT", "rt@mail.com", "password7"), "user";
+VALUES 
+("ID001", "DesiP", "dp@mail.com", "password!", "admin", "2000-01-01"),
+("ID002", "HannahC", "hc@mail.com", "password2", "admin", "1999-02-02"),
+("ID003", "KatF", "kf@mail.com", "password3", "user", "1998-03-03"),
+("ID004", "KoniB", "kb@mail.com", "password4)", "user", "1997-04-04"),
+("ID005", "MahumK", "mk@mail.com", "password5", "user", "1996-05-05"),
+("ID006", "MariaP", "mp@mail.com", "password6", "user", "1995-06-06"),
+("ID007", "RahaT", "rt@mail.com", "password7", "user", "1995-07-07")
+-- ("ID008", "SusanQ", "sq@mail.com", "password8", "user", "2010-08-08"); --this is a test of underaged user
+;
 
-INSERT INTO Search_Results
+CREATE TABLE Favourites (
+    faves_id VARCHAR(8) PRIMARY KEY,
+    user_id VARCHAR(5) NOT NULL,
+	activity ENUM("Pub/bar", "Cinema", "Bakery") NOT NULL,
+    latitude DECIMAL(11, 8),  -- Since latitude ranges from -180 to +180 (degrees), a DECIMAL(11,8) is appropriate.
+    longitude DECIMAL(11, 8), -- Since longtitude ranges from -180 to +180 (degrees), a DECIMAL(11,8) is appropriate.
+    time_block VARCHAR(9) NOT NULL, -- Python code will need to present time block options e.g. 0900-1200...alt ENUM 
+    -- times DATETIME,
+    venue_id VARCHAR(10) NOT NULL, -- if no review function, no need for venue_id, and remove from insert into also
+    vegan BOOLEAN DEFAULT FALSE,
+    gluten_free BOOLEAN DEFAULT FALSE,
+    alcohol_free BOOLEAN DEFAULT FALSE,
+    dog_friendly BOOLEAN DEFAULT FALSE,
+	CONSTRAINT FK_Favourites_id FOREIGN KEY (user_id) REFERENCES Users (user_id)
+    );
+    
+INSERT INTO Favourites (faves_id, user_id, latitude, longitude, times, activity, venue_id, vegan, gluten_free, alcohol_free, dog_friendly)
 VALUES
--- UserId, Lat_Lon, Times, Activity, Venue_Id, Vegan, Alcohol_Free, Dog_Friendly
-(ID001, ),
-(ID002, , ),
-(ID003, , ),
-(ID004, , ),
+("FAV00001", "ID001", 40.748817, -73.985428, "2024-11-16 14:30:00", "Pub/bar", "VENUE001", TRUE, TRUE, FALSE, TRUE); 
 
-INSERT INTO Messages
-VALUES
-(ID001, ID002, ID001, DateTime, "Message1", Y), 
-(ID002, ID001, ID001, DateTime, "Message2", Y), 
-(ID001, ID002, ID001, DateTime, "Message3", N);
+  CREATE TABLE Notification (
+  notification_id VARCHAR(8) PRIMARY KEY,
+  user_id VARCHAR(5), 
+  user_name VARCHAR(20) UNIQUE, -- discuss/explore: how to differentiate between the two users ...may not need if same notification to users 
+  date_time TIMESTAMP, 
+  notification VARCHAR(240) NOT NULL, 
+  -- notification_read BOOLEAN NOT NULL), -- maybe a stretch: if there is a way to confirm/reject, then notify contact details 
+  CONSTRAINT FK_Messages_uid FOREIGN KEY (user_id) REFERENCES Users (user_id),
+  CONSTRAINT FK_Messages_uname FOREIGN KEY (user_name) REFERENCES Users (user_name)
+    ); -- possibly, this should be separate and instead have a sent? (/additionally)
